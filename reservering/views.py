6ed493c26b2632dee1cfb498c94e3c1a33152a20
LucAndries <luc@kroanworks.be>
2025-10-21 17:59:28 +0200

    Update views.py

--- reservering/views.py
+++ reservering/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
-def kalender(request):
-    return render(request, 'reservering/kalender.html')
+from .forms import ReserveringForm
 
 def reservering_formulier(request):
-    return render(request, 'reservering/reservering_formulier.html')
+    if request.method == 'POST':
+        form = ReserveringForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'bevestiging.html')  # Bevestiging na succesvolle reservering
+    else:
+        form = ReserveringForm()
+    return render(request, 'reservering_formulier.html', {'form': form})
+
+def kalender(request):
+    return render(request, 'kalender.html')  # Je kalenderpagina
