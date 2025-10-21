ce94ded90413ec8361d0fd19c6979eaea0bf2a87
LucAndries <luc@kroanworks.be>
2025-10-21 19:12:59 +0200

    Update urls.py

--- reservering/urls.py
+++ reservering/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.reservering_formulier, name='reservering_formulier'),  # URL voor het formulier
-    path('kalender/', views.kalender, name='kalender'),  # URL voor de kalender
+    path('form/', views.reservering_formulier, name='reservering_formulier'),
+    path('bevestiging/', views.bevestiging, name='bevestiging'),  # Bevestigingspagina
 ]
